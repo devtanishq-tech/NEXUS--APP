53b6cc82ff74e733ecea9170baa042b8353682c7
Tanishq <tanishqcoc24@gmail.com>
2026-03-27 16:35:22 +0530

    fix the cookie error

--- backend/server.js
+++ backend/server.js
@@ -67,8 +67,8 @@ const sessionConfig = {
   }),
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
   },
   name: "sessionId", // Obscure default cookie name
